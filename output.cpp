5e66741b461894152e80725422a7a701e48f3266
jisha-113 <102210909+jisha-113@users.noreply.github.com>
2022-03-23 14:18:33 +0530

    Add files via upload

---
+++ output.cpp
@@ -0,0 +1,8 @@
+Enter the No: Of Employee 
+4
+The goodies selected for distribution are
+Fitbit Plus : 7980
+IPods : 22349
+MIBand : 999
+Cult Pass : 2799
+Difference between the chosen goodie with highest price and the lowest price is 21350
